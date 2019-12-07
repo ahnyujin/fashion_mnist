33952be4d3e44a9234df8e596b02b2dc4415bcac
ahnyujin <auj97@naver.com>
2019-12-07 16:25:31 +0900

    [task1][add relu dense and add dropout(0.5)]

--- FashionMNIST_CNN.py
+++ FashionMNIST_CNN.py
@@ -80,7 +80,8 @@ model.add(Conv2D(filters=32, kernel_size=(3,3),
                  activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
-model.add(Dense(128,activation='relu'))
+model.add(Dense(256,activation='relu'))
+model.add(Dropout(0.5))
 model.add(Dense(10,activation='softmax'))
 
 print(model.summary())
