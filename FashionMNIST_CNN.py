b3fbe86a35e2884215d6e82cd7d71923942890ca
ahnyujin <auj97@naver.com>
2019-12-07 17:23:17 +0900

    [task9][8.94%였던 cnn의 모델을 적용시켰는데 자꾸 떨어짐]

--- FashionMNIST_CNN.py
+++ FashionMNIST_CNN.py
@@ -90,6 +90,7 @@ model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Flatten())
 model.add(Dense(256,activation='relu'))
+model.add(Dropout(0.25)
 model.add(Dense(10,activation='softmax'))
 
 print(model.summary())
